ebe6f5dfe62cfddbca13bafeda660a470e47fc57
Andrew Nesbitt <andrew.nesbitt@github.com>
2014-01-04 14:23:01 +0000

    Simplified example

--- example.js
+++ example.js
@@ -3,19 +3,8 @@ var Webcam = require('./index.js')
 var webcam = new Webcam('192.168.1.178', 'admin', '123456')
 
 webcam.leftUp()
+
 setTimeout(function(){
+  webcam.stop()
   webcam.snapshot('leftUp.png')
-  webcam.leftDown()
-  setTimeout(function(){
-    webcam.snapshot('leftDown.png')
-    webcam.rightUp()
-    setTimeout(function(){
-      webcam.snapshot('rightUp.png')
-      webcam.rightDown()
-      setTimeout(function(){
-        webcam.snapshot('rightDown.png')
-        webcam.stop()
-      }, 2000)
-    }, 2000)
-  }, 2000)
-}, 2000)
+}, 5000)
